a3905174c63966d2d64e854e7234775074eed26c
khaled-Abdelslam <kabdelsalam81@yahoo.com>
2025-03-13 23:19:42 +0200

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Layout from './Components/Layout/Layout'
 import { createBrowserRouter, RouterProvider ,createHashRouter } from 'react-router-dom'
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-  base:'/Fresh-Cart/',
+ 
 
   
 })
